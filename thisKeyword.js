796628b30f115ddde585091f5f13782351bfab66
Marhatta <vishal.marhatta@gmail.com>
2020-05-04 16:12:29 +0530

    :recycle: Refactor

--- thisKeyword.js
+++ thisKeyword.js
@@ -8,7 +8,7 @@
 let obj = {
     property1:'property1',
     property2:'property2',
-    myFunc: function(){
+    myFunc(){
         console.log(this);
         // {
         //     property1: 'property1',
@@ -21,10 +21,14 @@ let obj = {
 
         //Now the above statement makes sense.
         //"this" is the object("obj" in our case)that the function is a property of. 
+
+        //Now if you want to access any property of the current object inside here
+        //you can do something like this
+        console.log('property1 -> ',this.property1);
     }
 }
 
-console.log(obj.myFunc());
+obj.myFunc();
 
 //In case of browser, initially "this" points to the global "window" object
 // To check run console.log(this) in browser console.
